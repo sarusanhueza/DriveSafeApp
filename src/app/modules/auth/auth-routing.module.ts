5c8f7abed691d6003cebbf7fe0cc344396269d3a
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2023-08-31 13:13:18 -0300

    cambio ruta

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './page/registro/registro.component';
 
 const routes: Routes = [
-  
+  {path: "registro", component: RegistroComponent}
 ];
 
 @NgModule({
