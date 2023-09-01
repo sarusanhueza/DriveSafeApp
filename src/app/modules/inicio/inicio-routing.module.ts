1b76bf8fdd19252c931217800ee1dc7d53ab4a85
Thiara Palma <thiarapalma233@gmail.com>
2023-09-01 08:13:32 -0300

    instale bootstrap y comence con la programacion del carrusel

--- src/app/modules/inicio/inicio-routing.module.ts
+++ src/app/modules/inicio/inicio-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageComponent } from './page/page.component';
-
 const routes: Routes = [
-{path: '', component: PageComponent  }
 ];
 
 @NgModule({
--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
 import { InicioComponent } from './page/inicio/inicio.component';
+import { CarruselComponent } from './componentes/carrusel/carrusel.component';
 
 
 @NgModule({
   declarations: [
-    InicioComponent
+    InicioComponent,
+    CarruselComponent
   ],
   imports: [
     CommonModule,
