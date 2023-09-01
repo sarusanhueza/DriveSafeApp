ee75562c45088fcd24b6ffd63d6788934fc85a10
sarusanhueza <sarusanhueza2@gmail.com>
2023-09-01 10:41:58 -0300

    se integro firebase y se agregaron los environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCkjw9-oakbr8_1rfLQIFx8Ja4unjCN7bo",
+    authDomain: "drivesafe-ff1b0.firebaseapp.com",
+    projectId: "drivesafe-ff1b0",
+    storageBucket: "drivesafe-ff1b0.appspot.com",
+    messagingSenderId: "669426415695",
+    appId: "1:669426415695:web:5768968390991ea4f3923a",
+    measurementId: "G-8KF1EK6RSW"
+  }
 };
 
 /*
