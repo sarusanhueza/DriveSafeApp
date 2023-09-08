9fdac57dc4a0e1c7b8ad473489481e4df332907a
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2023-09-08 13:14:54 -0300

    comienzo del registro

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './page/login/login.component';
 import { RegistroComponent } from './page/registro/registro.component';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { RegistroComponent } from './page/registro/registro.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    IonicModule
   ]
 })
 export class AuthModule { }
