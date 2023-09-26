2e1cd465d74cc9cb383c8c42b1cec7a95adf60fd
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2023-09-26 19:01:48 -0300

    cambios en el html

--- src/app/modules/auth/page/registro/registro.component.ts
+++ src/app/modules/auth/page/registro/registro.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, importProvidersFrom } from '@angular/core';
 
+
+
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss'],
 })
 export class RegistroComponent  implements OnInit {
+ 
 
   constructor() { }
 
