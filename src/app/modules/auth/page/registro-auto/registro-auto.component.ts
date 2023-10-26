c07b524da8a2b153a26f38f8d1cbc06a9b12e406
ThiaraPalma <thiarapalma233@gmail.com>
2023-10-25 23:59:39 -0300

    html listo

---
+++ src/app/modules/auth/page/registro-auto/registro-auto.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-auto',
+  templateUrl: './registro-auto.component.html',
+  styleUrls: ['./registro-auto.component.scss'],
+})
+export class RegistroAutoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
