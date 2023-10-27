684466024f1afd6c469df1cba6af3bcb3dceecff
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2023-10-27 13:33:40 -0300

    configuramos ruta configuracion

--- src/app/modules/inicio/inicio-routing.module.ts
+++ src/app/modules/inicio/inicio-routing.module.ts
@@ -1,8 +1,10 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './page/inicio/inicio.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
 const routes: Routes = [
-{path: 'inicio', component: InicioComponent  }
+{path: 'inicio', component: InicioComponent  },
+{path: 'configuracion', component: ConfiguracionComponent}
 ];
 
 @NgModule({
--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -4,18 +4,20 @@ import { CommonModule } from '@angular/common';
 import { InicioRoutingModule } from './inicio-routing.module';
 import { InicioComponent } from './page/inicio/inicio.component';
 import { CarruselComponent } from './componentes/carrusel/carrusel.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
 
 
 @NgModule({
   declarations: [
-    InicioComponent, CarruselComponent
+    InicioComponent, CarruselComponent, ConfiguracionComponent
   ],
   imports: [
     CommonModule,
     InicioRoutingModule,
   ],
   exports:[
-    CarruselComponent
+    CarruselComponent,
+    ConfiguracionComponent
   ]
 })
 export class InicioModule { }
