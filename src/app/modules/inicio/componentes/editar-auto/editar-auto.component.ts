0e3cd0da6e21d22fac79e762f55b70b64cc66df6
ThiaraPalma <thiarapalma233@gmail.com>
2023-11-05 21:02:14 -0300

    con problemas

--- src/app/modules/inicio/componentes/editar-auto/editar-auto.component.ts
+++ src/app/modules/inicio/componentes/editar-auto/editar-auto.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonModal } from '@ionic/angular';
-import {OverlayEventDetail} from '@ionic/core/components'
+import { OverlayEventDetail } from '@ionic/core/components'
 import { Vehiculo } from 'src/app/models/vehiculo';
 
 @Component({
@@ -13,13 +13,13 @@ import { Vehiculo } from 'src/app/models/vehiculo';
 export class EditarAutoComponent {
   @ViewChild(IonModal)
   modal!: IonModal;
-  
 
   message = '';
   name: string | undefined;
-  message1= '';
-  name1: string | undefined;
-    //confirmaciones o cancelaciones pertenecientes a NOMBRE
+
+
+
+  //confirmaciones o cancelaciones pertenecientes a NOMBRE
   cancel() {
     this.modal.dismiss(null, 'cancel');
   }
@@ -27,18 +27,8 @@ export class EditarAutoComponent {
   confirm() {
     this.modal.dismiss(this.name, 'confirm');
   }
-    
-    //confirmaciones o cancelaciones pertenecientes a PATENTE
 
-    cancel1() {
-      this.modal.dismiss(null, 'cancel1');
-    }
-  
-    confirm1() {
-      this.modal.dismiss(this.name, 'confirm1');
-    }
-
-   //cambiar por cada propiedad
+  //retornando confirmacion del NOMBRE 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
     if (ev.detail.role === 'confirm') {
@@ -46,15 +36,71 @@ export class EditarAutoComponent {
     }
   }
   
+  message1 = '';
+  patente: string | undefined;
+  //confirmaciones o cancelaciones pertenecientes a PATENTE
 
+  cancel1() {
+    this.modal.dismiss(null, 'cancel1');
+  }
+
+  confirm1() {
+    this.modal.dismiss(this.patente, 'confirm1');
+  }
+
+  //retornando confirmacion de la PATENTE
   onWillDismiss1(event1: Event) {
     const ev = event1 as CustomEvent<OverlayEventDetail<string>>;
     if (ev.detail.role === 'confirm1') {
       this.message1 = ` ${ev.detail.data}!`;
     }
   }
- 
 
- 
+   //MARCA
+  message2 = '';
+  marca: string | undefined;
+
+  //confirmaciones o cancelaciones pertenecientes a MARCA
+
+  cancel2() {
+    this.modal.dismiss(null, 'cancel2');
+  }
+
+  confirm2() {
+    this.modal.dismiss(this.marca, 'confirm2');
+  }
+
+  //retornando confirmacion de la MARCA
+  onWillDismiss2(event2: Event) {
+    const ev = event2 as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm2') {
+      this.message2 = ` ${ev.detail.data}!`;
+    }
+  }
+
+  //TIPO DE COMBUSTIBLE
+  message3 = '';
+  tipocombustible: string | undefined;
+
+  //confirmaciones o cancelaciones pertenecientes a TIPO COMBUSTIBLE
+
+  cancel3() {
+    this.modal.dismiss(null, 'cancel3');
+  }
+
+  confirm3() {
+    this.modal.dismiss(this.tipocombustible, 'confirm3');
+  }
+
+
+  //retornando confirmacion del TIPO DE COMBUSTIBLE
+  onWillDismiss3(event3: Event) {
+    const ev = event3 as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm3') {
+      this.message3 = ` ${ev.detail.data}!`;
+    }
+  }
+
+
 
 }
