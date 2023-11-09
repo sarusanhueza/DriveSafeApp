4ff5d6e1ff1e7ae46575faf357e6bb646e04a535
toia-19 <vickingiediaz19@gmail.com>
2023-11-09 09:19:52 -0300

    los valores ingresados en editarAuto ya se pueden mostrar

--- src/app/modules/inicio/componentes/editar-auto/editar-auto.component.ts
+++ src/app/modules/inicio/componentes/editar-auto/editar-auto.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components'
-import { Vehiculo } from 'src/app/models/vehiculo';
 
 @Component({
   selector: 'app-editar-auto',
@@ -15,7 +14,7 @@ export class EditarAutoComponent {
   modal!: IonModal;
 
   message = '';
-  name: string | undefined;
+  nombre: string | undefined;
   patente: string | undefined;
   marca: string | undefined;
   tcombustible: string | undefined;
@@ -27,22 +26,28 @@ export class EditarAutoComponent {
   }
 
   confirm() {
-    this.modal.dismiss(this.name,'confirm'); 
-    this.modal.dismiss(this.patente,'confirm'); 
-    this.modal.dismiss(this.marca,'confirm'); 
-    this.modal.dismiss(this.tcombustible,'confirm'); 
+    // encerramos en una constante verificados a todos los valores ingresados
+    const verificados = {
+      nombre: this.modal.dismiss(this.nombre,'confirm'),
+      patente: this.modal.dismiss(this.patente,'confirm'),
+      marca: this.modal.dismiss(this.marca,'confirm'),
+      tcombustible: this.modal.dismiss(this.tcombustible,'confirm'),
+    }
+
+    return verificados.nombre, verificados.patente, verificados.marca, verificados.tcombustible;
   }
-  
 
   //retornando confirmacion de propiedades
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
     if (ev.detail.role === 'confirm') {
-      this.message = ` ${ev.detail.data}`;
+      // this.message = ` ${ev.detail.data}`;
+      this.showAllValues()
     }
   }
-  
 
-
-
+  // showAllValues concatena todos los valores que fueron ingresados y los muestra
+  showAllValues(){
+    this.message = `Nombre: ${this.nombre}, Patente: ${this.patente}, Marca: ${this.marca}, Tipo de combustible: ${this.tcombustible}`;
+  }
 }
