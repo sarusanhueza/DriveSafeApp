4e306cdcf4fad24dad3a9acf53f5e39787916b0d
ThiaraPalma <thiarapalma233@gmail.com>
2023-11-20 00:27:36 -0300

    importando modulo (IonicModule) para que componentes prevenientes de ionic, funcionen

--- src/app/modules/inicio-admin/inicio-admin.module.ts
+++ src/app/modules/inicio-admin/inicio-admin.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InicioAdminRoutingModule } from './inicio-admin-routing.module';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    InicioAdminRoutingModule
+    InicioAdminRoutingModule,
+    //funcionamiento componentes ionic
+    IonicModule
   ]
 })
 export class InicioAdminModule { }
