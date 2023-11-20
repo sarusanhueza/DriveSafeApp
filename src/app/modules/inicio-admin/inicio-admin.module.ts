b77cb8a4fb69aeec5930565838eeebeb02980dd7
ThiaraPalma <thiarapalma233@gmail.com>
2023-11-20 00:30:17 -0300

    importando y exportando componente(RecomocimeintoComponent)

--- src/app/modules/inicio-admin/inicio-admin.module.ts
+++ src/app/modules/inicio-admin/inicio-admin.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { InicioAdminRoutingModule } from './inicio-admin-routing.module';
 import { IonicModule } from '@ionic/angular';
+//componentes
+import { ReconocimientoComponent } from './componentes/reconocimiento/reconocimiento.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ReconocimientoComponent],
   imports: [
     CommonModule,
     InicioAdminRoutingModule,
     //funcionamiento componentes ionic
     IonicModule
+  ],
+  exports:[
+   ReconocimientoComponent
   ]
 })
 export class InicioAdminModule { }
