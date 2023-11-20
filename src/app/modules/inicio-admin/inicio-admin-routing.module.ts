522961c9a92ad5fd4d37cf40323ff6be6dc1889c
ThiaraPalma <thiarapalma233@gmail.com>
2023-11-20 00:23:43 -0300

    creando ruta hija (reconocimiento) de ruta padre(inicio-admin)

--- src/app/modules/inicio-admin/inicio-admin-routing.module.ts
+++ src/app/modules/inicio-admin/inicio-admin-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioAdminModule } from './inicio-admin.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'reconocimiento', component:InicioAdminModule}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
