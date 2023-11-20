0dbbc8f7d97e7d644b4a8a4e1a4bfd0a627669fe
ThiaraPalma <thiarapalma233@gmail.com>
2023-11-20 00:39:15 -0300

    arreglando un error de la ruta hija del modulo inicio-admin (reconocimiento)

--- src/app/modules/inicio-admin/inicio-admin-routing.module.ts
+++ src/app/modules/inicio-admin/inicio-admin-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioAdminModule } from './inicio-admin.module';
+import { ReconocimientoComponent } from './componentes/reconocimiento/reconocimiento.component';
 
 const routes: Routes = [
-  {path:'reconocimiento', component:InicioAdminModule}
+  {path:'reconocimiento', component:ReconocimientoComponent}
 ];
 
 @NgModule({
