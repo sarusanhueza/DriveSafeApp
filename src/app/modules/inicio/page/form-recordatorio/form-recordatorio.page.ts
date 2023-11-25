3c052820996ff88e24f74ffcc50f233af0e4db0a
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2023-11-25 18:40:13 -0300

    funcionalidades del crud

--- src/app/modules/inicio/page/form-recordatorio/form-recordatorio.page.ts
+++ src/app/modules/inicio/page/form-recordatorio/form-recordatorio.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { CrudService } from '../../services/crud.service';
+import { FormControl, FormGroup, Validators, FormsModule } from '@angular/forms';
+import { Recordatorio } from 'src/app/models/Mrecordatorio';
+
 @Component({
   selector: 'app-form-recordatorio',
   templateUrl: './form-recordatorio.page.html',
@@ -7,9 +12,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormRecordatorioPage implements OnInit {
 
-  constructor() { }
+  coleccionRecordario: Recordatorio[] = [];
+  recordatorioSelec!: Recordatorio;
 
-  ngOnInit() {
+  Mrecordatorio = new FormGroup({
+    titulo: new FormControl('Recordatorio',Validators.required),
+    fecha: new FormControl('', Validators.required),
+    hora: new FormControl('', Validators.required),
+    nombreEvento: new FormControl('', Validators.required),
+    
+  })
+
+  col: any;
+
+  constructor(
+    public router: Router,
+    public servicioCrud : CrudService,
+  ) { }
+
+  ngOnInit(): void {
+    this.servicioCrud.obtenerRecordatorio().subscribe (recordatorio =>{
+      this.coleccionRecordario = recordatorio;
+    })
   }
+
+  async agregarRecordatorio (){
+    console.log("hola")
+    //if(this.Mcombustible.valid){
+      let nuevoRecordatorio : Recordatorio = {
+        uid: '',
+        titulo: this.Mrecordatorio.value.titulo!,
+        fecha: this.Mrecordatorio.value.fecha!,
+        hora: this.Mrecordatorio.value.hora!,
+        nombreEvento: this.Mrecordatorio.value.nombreEvento!,
+       
+       
+      };
+      console.log(nuevoRecordatorio);
+      const valor = await this.servicioCrud.crearRecordatorio(nuevoRecordatorio);
+      console.log(valor)
+  
+      //}
+      //else{
+      //  console.log(this.Mcombustible)
+     // }
+    }
+
+    mostrarEditar(recordatorioSelec: Recordatorio){
+      this.recordatorioSelec = recordatorioSelec;
+
+      this.Mrecordatorio.setValue({
+        titulo: recordatorioSelec.titulo,
+        fecha: recordatorioSelec.fecha,
+        hora: recordatorioSelec.hora,
+        nombreEvento: recordatorioSelec.nombreEvento,
+        
+        
+      })
+    }
+
+    editarRecordatorio(){
+      let datos: Recordatorio = {
+        uid: this.recordatorioSelec.uid,
+
+        titulo: this.Mrecordatorio.value.titulo!,
+        fecha: this.Mrecordatorio.value.fecha!,
+        hora: this.Mrecordatorio.value.hora!,
+        nombreEvento: this.Mrecordatorio.value.nombreEvento!,
+        
+
+      }
+
+
+      this.servicioCrud.modificarRecordatorio(this.recordatorioSelec.uid, datos)
+    }
+
+    mostrarBorrar(recordatorioSelec: Recordatorio){
+      //this.ModalVisibleCombustible = true;
+      this.recordatorioSelec = recordatorioSelec;
+    }
+
+    borrarGastos(){
+      this.servicioCrud.eliminarRecordatorio(this.recordatorioSelec.uid);
+
+    }
+
 
 }
