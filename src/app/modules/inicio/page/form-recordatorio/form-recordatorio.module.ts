896f1abbe7c9f30bc5c8573de0444cb73302637a
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2023-11-25 16:48:27 -0300

    importo form module y form reactive

--- src/app/modules/inicio/page/form-recordatorio/form-recordatorio.module.ts
+++ src/app/modules/inicio/page/form-recordatorio/form-recordatorio.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,12 @@ import { FormRecordatorioPage } from './form-recordatorio.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    FormRecordatorioPageRoutingModule
+    FormRecordatorioPageRoutingModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [FormRecordatorioPage]
 })
