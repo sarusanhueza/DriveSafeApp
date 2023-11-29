0ba4ee820978ebc6e83dc029e3b72637a6cb1de0
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2023-11-29 18:57:15 -0300

    creamos componente menu usuarios

---
+++ src/app/modules/inicio-admin/componentes/menu-usuarios/menu-usuarios.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-usuarios',
+  templateUrl: './menu-usuarios.component.html',
+  styleUrls: ['./menu-usuarios.component.scss'],
+})
+export class MenuUsuariosComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
